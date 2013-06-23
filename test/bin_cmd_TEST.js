428ef2708b2546fa1cb2769b0fb5b76d847f5661
asciidisco <public@asciidisco.com>
2013-06-23 22:17:36 +0200

    Mocha boilerplate

---
+++ test/bin_cmd_TEST.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var expect = require('chai').expect;
+
+describe('dalek-cli', function() {
+
+  it('i should think of proper testing', function(){
+    expect('cmd').to.equal('cmd');
+  });
+
+});
